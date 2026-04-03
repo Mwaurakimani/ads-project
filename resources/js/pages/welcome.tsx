a34bb9ed13fb81a1870b2c860bed3ca4d5f22f96
Peter Mwaura <kimmwaus@gmail.com>
2026-04-03 14:39:18 +0300

    initial commit

---
+++ resources/js/pages/welcome.tsx
@@ -0,0 +1,31 @@
+import { Head, Link, usePage } from '@inertiajs/react';
+// import { dashboard, login, register } from '@/routes';
+
+export default function Welcome({
+    // canRegister = true,
+}: {
+    canRegister?: boolean;
+}) {
+    // const { auth } = usePage().props;
+
+    navigator.geolocation.getCurrentPosition((pos) => {
+        console.log(`Lat: ${pos.coords.latitude}, Lon: ${pos.coords.longitude}`);
+    }, (err) => {
+        console.error(`Error: ${err.message}`);
+    });
+
+
+
+    return (
+        <>
+            {/*<Head title="Welcome">*/}
+            {/*    <link rel="preconnect" href="https://fonts.bunny.net" />*/}
+            {/*    <link*/}
+            {/*        href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600"*/}
+            {/*        rel="stylesheet"*/}
+            {/*    />*/}
+            {/*</Head>*/}
+            <p className={'bg-blue-400'}>working</p>
+        </>
+    );
+}
